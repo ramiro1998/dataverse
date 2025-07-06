cba6dba5e2d48eff498c90c812102be4539af77d
ramiro1998 <ramiroredona@gmail.com>
2025-07-06 02:31:20 -0300

    redirects netlify

---
+++ redirects.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+const redirectsPath = path.join(
+    __dirname,
+    "dist",
+    "dataverse",
+    "_redirects"
+);
+const redirectsContent = `/*    /index.html   200\n`;
+fs.mkdirSync(path.dirname(redirectsPath), { recursive: true });
+fs.writeFileSync(redirectsPath, redirectsContent, "utf8");
