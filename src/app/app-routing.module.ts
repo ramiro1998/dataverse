a0fe8a91c5e63fff874d702ef251e573773fbd18
ramiro1998 <ramiroredona@gmail.com>
2025-07-06 20:25:55 -0300

    fix app routing, cleaning code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
   { path: 'people', loadChildren: () => import('./people/people.module').then(m => m.PeopleModule) },
   { path: 'starships', loadChildren: () => import('./starship/starship.module').then(m => m.StarshipModule) },
   { path: 'planets', loadChildren: () => import('./planet/planet.module').then(m => m.PlanetModule) },
   { path: 'species', loadChildren: () => import('./specie/specie.module').then(m => m.SpecieModule) },
   { path: 'vehicles', loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule) },
   { path: 'films', loadChildren: () => import('./film/film.module').then(m => m.FilmModule) },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
